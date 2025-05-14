c11c8db7e4eb09d4ec586451c43b69aa153fb008
MARKOZ <zadro.marko@nsoft.com>
2025-05-14 14:38:59 +0200

    test

--- src/main/java/com/dockerforjavadevelopers/hello/Application.java
+++ src/main/java/com/dockerforjavadevelopers/hello/Application.java
@@ -1,20 +1,25 @@
 package com.dockerforjavadevelopers.hello;
 
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @Configuration
 @EnableAutoConfiguration
 @ComponentScan
+@RestController
 public class Application {
-    
+
     public static void main(String[] args) {
         ApplicationContext ctx = SpringApplication.run(Application.class, args);
-        
     }
 
+    @GetMapping("/healthz")
+    public String healthz() {
+        return "OK";
+    }
 }
