667eba229268c7870220d263569a9c092d9efdd5
MARKOZ <zadro.marko@nsoft.com>
2025-05-16 10:16:29 +0200

    test

--- src/main/java/com/dockerforjavadevelopers/hello/Application.java
+++ src/main/java/com/dockerforjavadevelopers/hello/Application.java
@@ -37,3 +37,45 @@ public class Application {
         return "Log sent to file.";
     }
 }
+
+
+
+//   ovo dole je da kad se logs ruta pozove da ide na stdout na terminal bez da pise u volume a ovo iznad je primjer da pise u volume gdje sad dodao i application.properties file
+
+
+// package com.dockerforjavadevelopers.hello;
+
+// import org.springframework.boot.SpringApplication;
+// import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+// import org.springframework.context.ApplicationContext;
+// import org.springframework.context.annotation.ComponentScan;
+// import org.springframework.context.annotation.Configuration;
+// import org.springframework.web.bind.annotation.GetMapping;
+// import org.springframework.web.bind.annotation.RestController;
+
+// import java.util.UUID;
+
+// @Configuration
+// @EnableAutoConfiguration
+// @ComponentScan
+// @RestController
+// public class Application {
+
+//     private static final String APP_NAME = "onboardingtutorial";
+
+//     public static void main(String[] args) {
+//         ApplicationContext ctx = SpringApplication.run(Application.class, args);
+//     }
+
+//     @GetMapping("/healthz")
+//     public String healthz() {
+//         return "OK";
+//     }
+
+//     @GetMapping("/logs")
+//     public String logToConsole() {
+//         String uuid = UUID.randomUUID().toString();
+//         System.out.println("UUID: " + uuid + ", App: " + APP_NAME);
+//         return "Log sent to console.";
+//     }
+// }
